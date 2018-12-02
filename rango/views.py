6a6a7a070fe894bc74f530ed48c0e1615f9f010c
Jon <jonerra23@aim.com>
2018-12-02 13:39:22 -0500

    add requirements.txt

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import *
 
 def index(request):
+	# Query the database for a list of all categories currently stored
+	# Order the categories by no. of likes in descending order
+	# Retrieve the top 5 only or all if less than 5
+	# Place the list in our context_dict dictionary
+	# that will be passed on to the template engine
 
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+	category_list = Category.objects.order_by('-like')[:5]
+	context_dict = {'categories': category_list}
+
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
