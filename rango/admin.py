d6d70c51ed6b166ba6c855c4ac15f625bb3f5dab
Jon <jonerra23@aim.com>
2018-12-02 18:02:22 -0500

    add category pages

--- rango/admin.py
+++ rango/admin.py
@@ -5,5 +5,8 @@ class PageAdmin(admin.ModelAdmin):
 	list_display = ('title', 'category', 'url')
 	search_fields = ['title']
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -16,3 +16,31 @@ def index(request):
 
 def about(request):
 	return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+	# Create a context dictionary which we can pass
+	# to the templat rendering engine
+	context_dict = {}
+
+	try:
+		# Can we find a category name slug with the given name?
+		# If we can't, the .get() method raises a DoesNotExist exception
+		# so the .get() method returns one model instance or raises an exception.
+		category = Category.objects.get(slug=category_name_slug)
+
+		# Retrieve all of the associated pages.
+		# Note that filter() will return a list of pages objects or an empty list
+		pages = Page.objects.filter(category=category)
+
+		# Adds our results list to the template context under name pages.
+		context_dict['pages'] = pages
+
+		# We also add the category object from
+		# the database to the context dictionary.
+		# We'll use this in the template to verify that the category exists.
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+	return render(request, 'rango/category.html', context_dict)
